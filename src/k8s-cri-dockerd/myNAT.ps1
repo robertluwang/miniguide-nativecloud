6c2ca66c6f6ed3f0c3e32b6bf59222f96e0df130
Robert Wang <robert.lu.wang@gmail.com>
2024-04-15 12:06:22 -0400

    Update and rename myNAT.ps to myNAT.ps1

--- src/k8s-cri-dockerd/myNAT.ps1
+++ src/k8s-cri-dockerd/myNAT.ps1
@@ -1,12 +1,10 @@
-date /t
-time /t
+Get-Date
 
 Remove-VMSwitch -Name 'myNAT'
 New-VMSwitch -SwitchName 'myNAT' -SwitchType Internal
 
 Get-NetAdapter 'vEthernet (myNAT)' | Get-NetIPAddress | Remove-NetIPAddress -Confirm:$False; New-NetIPAddress -IPAddress 192.168.120.1 -PrefixLength 24 -InterfaceAlias 'vEthernet (myNAT)'; Get-NetNat | ? Name -Eq myNAT | Remove-NetNat -Confirm:$False; New-NetNat -Name myNAT -InternalIPInterfaceAddressPrefix 192.168.120.0/24
 
-date /t
-time /t
+Get-Date
 
 pause
