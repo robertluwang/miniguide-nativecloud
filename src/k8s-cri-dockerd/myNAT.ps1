2e3807aa4c3456830467cf58572732da9c50190f
Robert Wang <robert.lu.wang@gmail.com>
2024-04-21 22:26:02 -0400

    Update myNAT.ps1

--- src/k8s-cri-dockerd/myNAT.ps1
+++ src/k8s-cri-dockerd/myNAT.ps1
@@ -1,10 +1,30 @@
-Get-Date
+If ("myNAT" -in (Get-VMSwitch | Select-Object -ExpandProperty Name) -eq $FALSE) {
+    'Creating Internal-only switch "myNAT" on Windows Hyper-V host...'
 
-Remove-VMSwitch -Name 'myNAT'
-New-VMSwitch -SwitchName 'myNAT' -SwitchType Internal
+    New-VMSwitch -SwitchName "myNAT" -SwitchType Internal
 
-Get-NetAdapter 'vEthernet (myNAT)' | Get-NetIPAddress | Remove-NetIPAddress -Confirm:$False; New-NetIPAddress -IPAddress 192.168.120.1 -PrefixLength 24 -InterfaceAlias 'vEthernet (myNAT)'; Get-NetNat | ? Name -Eq myNAT | Remove-NetNat -Confirm:$False; New-NetNat -Name myNAT -InternalIPInterfaceAddressPrefix 192.168.120.0/24
+    New-NetIPAddress -IPAddress 192.168.120.1 -PrefixLength 24 -InterfaceAlias "vEthernet (myNAT)"
 
-Get-Date
+    New-NetNAT -Name "myNAT" -InternalIPInterfaceAddressPrefix 192.168.120.0/24
+}
+else {
+    'Internal-only switch "myNAT" for static IP configuration already exists; skipping'
+}
 
-pause
+If ("192.168.120.1" -in (Get-NetIPAddress | Select-Object -ExpandProperty IPAddress) -eq $FALSE) {
+    'Registering new IP address 192.168.120.1 on Windows Hyper-V host...'
+
+    New-NetIPAddress -IPAddress 192.168.120.1 -PrefixLength 24 -InterfaceAlias "vEthernet (myNAT)"
+}
+else {
+    'IP address "192.168.120.1" for static IP configuration already registered; skipping'
+}
+
+If ("192.168.120.0/24" -in (Get-NetNAT | Select-Object -ExpandProperty InternalIPInterfaceAddressPrefix) -eq $FALSE) {
+    'Registering new NAT adapter for 192.168.120.0/24 on Windows Hyper-V host...'
+
+    New-NetNAT -Name "myNAT" -InternalIPInterfaceAddressPrefix 192.168.120.0/24
+}
+else {
+    '"192.168.120.0/24" for static IP configuration already registered; skipping'
+}
